9771079466e2fd72bbf25198268a50ef92a219ea
ThomiPereyra <thpereyra@frba.utn.edu.ar>
2024-11-09 14:50:59 -0600

    clases view

--- escolares/src/App.jsx
+++ escolares/src/App.jsx
@@ -1,17 +1,28 @@
-import {BrowseRouter as Router, Route, Routes} from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import React from "react"
+import Home from "./views/Home"
+import Talleres from "./views/Talleres"
+import Informes from "./views/Informes"
+import Clases from "./views/Clases"
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+
 
 function App() {
 
   return (
+    <>
     <Router>
+      <Header/>
+      <Navbar/>
       <Routes>
-        <Route  path="/" element={<Home/>}/> 
-        <Route path="/talleres" element={<Talleres/>}/> 
+        <Route path="/" element={<Home/>}/> 
+         <Route path="/talleres" element={<Talleres/>}/> 
         <Route path="/informes" element={<Informes/>}/> 
-        <Route path="/clases" element={<Clases/>}/> 
+        <Route path="/clases" element={<Clases/>}/>  
       </Routes>
       
-    </Router>
+    </Router></>
   )
 }
 
---
+++ escolares/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return (
+    <>
+           <section className=" w-full bg-lime-500 p-8 ">
+                <h2 className=" text-2xl font-bold">ESCUELA RURAL N°12 </h2>
+           </section>
+    </>
+    )
+}
+
